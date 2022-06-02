5bfb7e97882e9f813b04e38a52aeaffa16777424
steve <steve@Alonso>
2022-06-02 07:39:58 -0600

    Tarea asignada en semana 3

---
+++ src/main/java/CentrosTuristicos/CentrosTuristicosApplication.java
@@ -0,0 +1,13 @@
+package CentrosTuristicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentrosTuristicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentrosTuristicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CentrosTuristicos/CentrosTuristicosApplicationTests.java
@@ -0,0 +1,13 @@
+package CentrosTuristicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentrosTuristicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
